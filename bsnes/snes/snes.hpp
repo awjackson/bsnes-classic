fd32f5235dd5edd046f7001566e0c469a901e83b
Alex W. Jackson <alex.w.jackson@gmail.com>
2014-09-11 14:18:46 +0000

    Version bump

--- bsnes/snes/snes.hpp
+++ bsnes/snes/snes.hpp
@@ -1,7 +1,7 @@
 namespace SNES {
   namespace Info {
     static const char Name[] = "bsnes-classic";
-    static const char Version[] = "073";
+    static const char Version[] = "073u1";
     static const unsigned SerializerSignature = 0x43545342; //'BSTC'
     static const unsigned SerializerVersion = 2;
   }
